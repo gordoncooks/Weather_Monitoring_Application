ae91fea37d3fe2084ac1108eee1115aa6dfe9ca7
Gordon <141590873+gordon-coding@users.noreply.github.com>
2024-06-20 12:19:02 +0200

    Add project files.

---
+++ Weather_Monitoring_Application/Sensor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Monitoring_Application
+{
+    // Purpose: Serve as a common bese for all sensor types
+    public abstract class Sensor
+    {
+        
+        // constructer
+        public string sensorType { get; set; }
+
+        // colect the data and log it
+        public abstract void CollectData();
+
+        // Log the data to the terminal
+        protected void LogData(string data)
+        {
+            Console.WriteLine($"{sensorType} Data: {data}");
+        }
+    }
+}
---
+++ Weather_Monitoring_Application/TemperatureSensor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Weather_Monitoring_Application
+{
+    // Purpose: Collect temperature data
+    public class TemperatureSensor : Sensor
+    {
+
+        // declare constance
+        double MINIMUM_TEMPERATURE = -15.00;
+        double MAXIMUM_TEMPERATURE = 40.00;
+
+
+        // constructer
+        public TemperatureSensor()
+        {
+            sensorType = "Temperature";
+        }
+
+        public override void CollectData()
+        {
+            // Create a random object
+            Random random = new Random();
+
+            while (true)
+            {
+                // generate a random temperature data (Simulating as if real time data)
+                double randomValue = random.NextDouble(); // makes the randum value
+                double temperature = MINIMUM_TEMPERATURE + ( randomValue * ( MAXIMUM_TEMPERATURE - MINIMUM_TEMPERATURE ) ); // make the temperature value using the random value and max and min values.
+
+
+                // Collect data every 1 second
+                Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+}
